6901bceff5cd4d45e975f97e5ac677bdcbe1c74a
shoyo-inokuchi <shoyoinokuchi@gmail.com>
2019-03-09 00:26:01 +0900

    Add initial files. Loads client, api, db with docker-compose up.

---
+++ api/entrypoint.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -e
+
+# Remove a potentially pre-existing server.pid for Rails.
+rm -f /myapp/tmp/pids/server.pid
+
+# Then exec the container's main process (what's set as CMD in the Dockerfile).
+exec "$@"
+
+
+# Reference: https://docs.docker.com/compose/rails/
