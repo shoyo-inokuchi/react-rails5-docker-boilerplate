b815e356a05312b77cf12d5c04accd9abd91c04a
shoyo-inokuchi <shoyoinokuchi@gmail.com>
2019-03-09 12:53:07 +0900

    Reconfigure ActionController inheritance and middleware

--- api/config/application.rb
+++ api/config/application.rb
@@ -31,5 +31,11 @@ module DockerApi
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    # Add for ActiveAdmin to function properly
+    config.middleware.use Rack::MethodOverride
+    config.middleware.use ActionDispatch::Flash
+    config.middleware.use ActionDispatch::Cookies
+    config.middleware.use ActionDispatch::Session::CookieStore
   end
 end
